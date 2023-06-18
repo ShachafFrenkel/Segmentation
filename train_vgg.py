a4f932b91ebbd05cdada9a10f23bd05bb2005a6c
John Doe <perymaypery@gmail.com>
2023-06-18 13:38:07 +0300

    image segmentation training for microsoft azure

---
+++ train_vgg.py
@@ -0,0 +1,30 @@
+# set the matplotlib backend so figures can be saved in the background
+import matplotlib
+matplotlib.use("Agg")
+# import the necessary packages
+from smallvggnet import SmallVGGNet
+from fit_generator import image_generator_train
+from sklearn.preprocessing import LabelBinarizer
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.optimizers import SGD
+from imutils import paths
+import matplotlib.pyplot as plt
+import numpy as np
+import argparse
+import random
+import pickle
+import cv2
+import os
+# construct the argument parser and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-d", "--dataset", required=True,
+	help="path to input dataset of images")
+ap.add_argument("-m", "--model", required=True,
+	help="path to output trained model")
+ap.add_argument("-l", "--label-bin", required=True,
+	help="path to output label binarizer")
+ap.add_argument("-p", "--plot", required=True,
+	help="path to output accuracy/loss plot")
+args = vars(ap.parse_args())
